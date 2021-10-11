cf09a3b0e8ba7c92684b63f5bf629998bf68ffb3
NatIm15 <90975868+NatIm15@users.noreply.github.com>
2021-10-11 11:34:15 -0500

    Add files via upload

---
+++ SprintIII/RUsuarioScript.js
@@ -0,0 +1,9 @@
+function mostraruserReq(script) {
+    var userRequisito = document.getElementById('uReq');
+    userRequisito.innerHTML = script;
+}
+
+function mostrarpReq(script) {
+    var pRequisito = document.getElementById('pReq');
+    pRequisito.innerHTML = script;
+}
